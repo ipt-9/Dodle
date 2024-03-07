ed149fc920b7ab436831013dd68749167b630669
Joel Erni <joel.erni@hispeed.ch>
2024-03-07 13:45:45 +0100

    Class location added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {LocationService} from "./location.service";
+import {NgForOf} from "@angular/common";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'untitled';
+  title = 'Test';
+  coords:any = []
+
+  constructor(protected LocationService:LocationService) {}
+
+  printCoord(){
+    this.coords = this.LocationService.watchPosition()
+  }
+
+  stopPrint(){
+    this.LocationService.stopWatch()
+    console.log(this.coords[this.coords.length -1])
+  }
+
+  protected readonly stop = stop;
 }
