d660bee12af3f7756795bc324b437c4b1e9b58fc
Joel Erni <joel.erni@hispeed.ch>
2024-03-17 15:30:32 +0100

    Page Routes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {LocationService} from "./location.service";
-import {HapticService} from "./haptic.service";
-import {DataService} from "./data.service";
+
 import {NgForOf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 
@@ -13,29 +11,6 @@ import {FormsModule} from "@angular/forms";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  title = 'Test';
-  coords:any = []
-  public distance: number = 0;
-  data_input = "";
+export class AppComponent{
 
-  constructor(protected LocationService:LocationService, protected HapticService:HapticService, protected dataService:DataService) {}
-
-  printCoord(){
-    this.coords = this.LocationService.watchPosition()
-  }
-
-  ngOnInit() {
-    this.LocationService.distanceFromCurrentPosition({timestamp:0, coords: {latitude: 47.17343,longitude: 8.094384,accuracy: 0, altitude: 0, altitudeAccuracy: 0, speed:0, heading:0}})
-  }
-
-  stopPrint(){
-    this.LocationService.stopWatch()
-  }
-
-  protected readonly stop = stop;
-  protected readonly Location = Location;
-  protected readonly GeolocationPosition = GeolocationPosition;
-  protected readonly Geolocation = Geolocation;
-  protected readonly Math = Math;
 }
