e5b1da8bd20d76d035f1d8614feecce4946f99ac
Joel Erni - Desktop <joel.erni@hispeed.ch>
2024-05-22 00:02:21 +0200

    Email button added and empty faq page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,15 @@ import {NotFoundPageComponent} from "./not-found-page/not-found-page.component";
 import {IntroductionComponent} from "./introduction/introduction.component";
 import {RoutedetailComponent} from "./routedetail/routedetail.component";
 import {FinishComponent} from "./finish/finish.component";
+import {FaqComponent} from "./faq/faq.component";
 
 export const routes: Routes = [
   { path: 'test', component: TestComponent},
+  { path: 'faq', component: FaqComponent},
   { path: 'route/:id', component: RouteComponent},
   { path: 'introduction/:id', component: IntroductionComponent},
   {path: 'routedetail/:id', component: RoutedetailComponent},
   {path: 'finish/:id', component: FinishComponent},
   { path: '', component: HomeComponent},
-  { path: ':q', component: HomeComponent},
   {path: '**', component: NotFoundPageComponent},
 ];
